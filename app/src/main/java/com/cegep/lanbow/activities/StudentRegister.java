887db6fffd090282c9e751963b143803b7688655
deepmal933 <deepmal944@gmail.com>
2020-09-29 18:55:20 -0400

    added input validation

--- app/src/main/java/com/cegep/lanbow/activities/StudentRegister.java
+++ app/src/main/java/com/cegep/lanbow/activities/StudentRegister.java
@@ -22,8 +22,21 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.DecimalMax;
+import com.mobsandgeeks.saripaar.annotation.DecimalMin;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Length;
+import com.mobsandgeeks.saripaar.annotation.Max;
+import com.mobsandgeeks.saripaar.annotation.Min;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
+import com.mobsandgeeks.saripaar.annotation.Pattern;
 
-public class StudentRegister extends AppCompatActivity {
+import java.util.List;
+
+public class StudentRegister extends AppCompatActivity implements Validator.ValidationListener {
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
@@ -31,8 +44,27 @@ public class StudentRegister extends AppCompatActivity {
     private ImageView backbtn;
     private LinearLayout Step1,Step2;
     private Button next,signup;
+    @NotEmpty
+    private EditText nameInput;
+    @NotEmpty
+    @Email
+    private EditText emailInput;
+    @Password(min = 6, scheme = Password.Scheme.ALPHA_NUMERIC_MIXED_CASE_SYMBOLS)
+    private EditText passInput;
 
-    private EditText nameInput,emailInput,passInput,idInput,phoneInput,addressInput;
+    @NotEmpty(message = "Student id shouldn't be empty")
+    @Length(min = 8,max = 8,message = "Student Id should be 8 digit")
+    private EditText idInput;
+
+    @NotEmpty(message = "Phone number shouldn't be empty")
+    @Pattern(regex = "^[0-9-]+$",message = "Enter valid phone number")
+    private EditText phoneInput;
+    @NotEmpty
+    private EditText addressInput;
+
+    private Validator validator;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +73,9 @@ public class StudentRegister extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         mDatabase = database.getReference();
+
+        validator = new Validator(this);
+        validator.setValidationListener(this);
 
         Step1 = findViewById(R.id.Step1);
         Step2 = findViewById(R.id.Step2);
@@ -57,50 +92,20 @@ public class StudentRegister extends AppCompatActivity {
         phoneInput = findViewById(R.id.phoneInput);
         addressInput = findViewById(R.id.addressInput);
 
+
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!nameInput.getText().equals("")&&!emailInput.getText().equals("")&&!passInput.getText().equals("")){
-                    Step1.setVisibility(View.GONE);
-                    Step2.setVisibility(View.VISIBLE);
-                }
-                else{
+                validator.validate();
 
-                }
             }
         });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!idInput.getText().equals("")&&!phoneInput.getText().equals("")&&!addressInput.getText().equals("")){
-                    mAuth.createUserWithEmailAndPassword(emailInput.getText().toString(), passInput.getText().toString())
-                            .addOnCompleteListener(StudentRegister.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        FirebaseUser user = mAuth.getCurrentUser();
-                                        Student student = new Student(nameInput.getText().toString(),user.getEmail(),idInput.getText().toString(),phoneInput.getText().toString(),addressInput.getText().toString());
-
-                                        database.getReference().child("Users").child(mAuth.getCurrentUser().getUid()).setValue(student).addOnSuccessListener(new OnSuccessListener<Void>() {
-                                            @Override
-                                            public void onSuccess(Void aVoid) {
-                                                Toast.makeText(StudentRegister.this, "sucess",
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        Toast.makeText(StudentRegister.this, "Authentication failed."+task.getException(),
-                                                Toast.LENGTH_SHORT).show();
-
-                                    }
-
-                                    // ...
-                                }
-                            });
-                }
+                validator.validate();
 
             }
         });
@@ -117,5 +122,60 @@ public class StudentRegister extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        if(Step1.getVisibility()== View.VISIBLE){
+            Step1.setVisibility(View.GONE);
+            Step2.setVisibility(View.VISIBLE);
+        }
+        else{
+            RegisterUser();
+        }
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void RegisterUser(){
+        mAuth.createUserWithEmailAndPassword(emailInput.getText().toString(), passInput.getText().toString())
+                .addOnCompleteListener(StudentRegister.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Student student = new Student(nameInput.getText().toString(),user.getEmail(),idInput.getText().toString(),phoneInput.getText().toString(),addressInput.getText().toString());
+
+                            database.getReference().child("Users").child(mAuth.getCurrentUser().getUid()).setValue(student).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(StudentRegister.this, "sucess",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(StudentRegister.this, "Authentication failed."+task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
