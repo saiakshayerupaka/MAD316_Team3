5627367123e2084f76617a8895b929715c188f3f
Prashant-Nannuru <prashanthreddy.1234@yahoo.in>
2020-09-29 12:29:04 -0400

    created student register screen

---
+++ app/src/main/java/com/cegep/lanbow/activities/StudentRegister.java
@@ -0,0 +1,29 @@
+package com.cegep.lanbow.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.cegep.lanbow.R;
+
+public class StudentRegister extends AppCompatActivity {
+
+    private ImageView backbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_register);
+
+        backbtn = findViewById(R.id.backbtn);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
