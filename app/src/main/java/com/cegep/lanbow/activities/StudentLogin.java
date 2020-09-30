862c4d76e402c4050d7f09a44973fdecd3a32a4a
Sai Akshay <erupakasaiakshay@gmail.com>
2020-09-29 21:14:59 -0400

    added login feature

--- app/src/main/java/com/cegep/lanbow/activities/StudentLogin.java
+++ app/src/main/java/com/cegep/lanbow/activities/StudentLogin.java
@@ -1,21 +1,56 @@
 package com.cegep.lanbow.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.cegep.lanbow.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class StudentLogin extends AppCompatActivity {
 
     private ImageView backbtn;
+    private FirebaseAuth mAuth;
+
+    private EditText emailInput,passInput;
+    private Button studentLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailInput = findViewById(R.id.emailInput);
+        passInput = findViewById(R.id.passInput);
+        studentLogin = findViewById(R.id.studentLogin);
+
+        studentLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signInWithEmailAndPassword(emailInput.getText().toString(),passInput.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+
+                        }
+                        else{
+
+                        }
+                    }
+                })
+
+            }
+        });
 
         backbtn = findViewById(R.id.backbtn);
 
