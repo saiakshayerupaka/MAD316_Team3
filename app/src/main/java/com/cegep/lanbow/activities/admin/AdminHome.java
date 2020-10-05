e7e1eaa9a33a835df99d62ea068698809a068c1e
harshavardhan <haiharshavardhan@gmail.com>
2020-10-05 16:20:21 -0400

    count the no of user using firebase

--- app/src/main/java/com/cegep/lanbow/activities/admin/AdminHome.java
+++ app/src/main/java/com/cegep/lanbow/activities/admin/AdminHome.java
@@ -1,5 +1,6 @@
 package com.cegep.lanbow.activities.admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
@@ -11,11 +12,16 @@ import android.widget.TextView;
 import com.cegep.lanbow.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class AdminHome extends AppCompatActivity {
 
     private ImageView backbtn;
     private TextView countUser,countItem;
+    private FirebaseDatabase database;
 
 
     @Override
@@ -23,10 +29,24 @@ public class AdminHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
+        database = FirebaseDatabase.getInstance();
+
 
         countUser = findViewById(R.id.countUser);
         countItem = findViewById(R.id.countItem);
 
+        database.getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                int i = (int) snapshot.getChildrenCount();
+                countUser.setText(String.valueOf(i));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
