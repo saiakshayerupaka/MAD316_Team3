1fe63bea882e2c6f1ec574ff215299d181f25f99
Amansehmi <Amansehmi10@gmail.com>
2020-10-07 09:28:06 -0700

    added firebase db to item activity

--- app/src/main/java/com/cegep/lanbow/activities/admin/Items.java
+++ app/src/main/java/com/cegep/lanbow/activities/admin/Items.java
@@ -1,14 +1,43 @@
 package com.cegep.lanbow.activities.admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.cegep.lanbow.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Items extends AppCompatActivity {
+
+    private Listview itemlist;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        itemlist = findViewById(R.id.item_list);
+
+        database = FirebaseDatabase.getInstance();
+
+        database.getReference().child("Items").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
     }
 }
