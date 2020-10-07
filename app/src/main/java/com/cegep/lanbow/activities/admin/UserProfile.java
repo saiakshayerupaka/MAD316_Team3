4940c30de58dd92d6651b5a0b4468dc20f752ca4
harshavardhan <haiharshavardhan@gmail.com>
2020-10-07 07:25:39 -0400

    linked ui with UserProfile activity

--- app/src/main/java/com/cegep/lanbow/activities/admin/UserProfile.java
+++ app/src/main/java/com/cegep/lanbow/activities/admin/UserProfile.java
@@ -3,14 +3,48 @@ package com.cegep.lanbow.activities.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.cegep.lanbow.R;
 
 public class UserProfile extends AppCompatActivity {
 
+    private TextView name,email,studentid,phone,address;
+    private Button block;
+    private ImageView back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile2);
+
+        back = findViewById(R.id.backbtn);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        name = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        studentid = findViewById(R.id.studentid);
+        phone = findViewById(R.id.phonenumber);
+        address = findViewById(R.id.address);
+
+        block = findViewById(R.id.block);
+
+        block.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
