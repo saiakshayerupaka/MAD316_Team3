43bc91479841312f56a6c94ea09de876578219a7
gopi chand <chandu114477@gmail.com>
2020-10-12 17:50:49 -0400

    call database and fecth list of item and pass it to the adapter

--- app/src/main/java/com/cegep/lanbow/activities/student/UserHome.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/UserHome.java
@@ -1,5 +1,6 @@
 package com.cegep.lanbow.activities.student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,14 +11,25 @@ import android.widget.ListView;
 
 import com.cegep.lanbow.R;
 import com.cegep.lanbow.adapters.HomeItemListAdapter;
+import com.cegep.lanbow.adapters.ItemlistAdapter;
+import com.cegep.lanbow.models.Item;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserHome extends AppCompatActivity {
     private ImageView profile;
     private FirebaseAuth mAuth;
+    private FirebaseDatabase database;
     private ListView listview;
     private HomeItemListAdapter homeItemListAdapter;
+    private List<Item> itemlist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,9 @@ public class UserHome extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
 
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        listview = findViewById(R.id.listview);
+
 
         FirebaseUser user = mAuth.getCurrentUser();
 
@@ -41,6 +56,27 @@ public class UserHome extends AppCompatActivity {
                 startActivity(new Intent(UserHome.this,UserProfile.class));
             }
         });
+
+        database.getReference().child("Items").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                itemlist.clear();
+                for(DataSnapshot snap : snapshot.getChildren()){
+                    itemlist.add(snap.getValue(Item.class));
+                }
+                homeItemListAdapter = new HomeItemListAdapter(UserHome.this,itemlist);
+                listview.setAdapter(homeItemListAdapter);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
     }
 }
