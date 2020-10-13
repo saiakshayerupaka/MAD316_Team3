92447770940a74c77d6d17bb1e41beb033008791
AmanSehmi <amansehmi10@gmail.com>
2020-10-12 23:44:51 -0400

    password reset activity update

--- app/src/main/java/com/cegep/lanbow/activities/student/PasswordReset.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/PasswordReset.java
@@ -3,14 +3,45 @@ package com.cegep.lanbow.activities.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.cegep.lanbow.R;
 
 public class PasswordReset extends AppCompatActivity {
 
+    private EditText pass,confirmpass;
+    private Button reset;
+    private ImageView back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_reset);
+
+        back = findViewById(R.id.backbtn);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        pass = findViewById(R.id.pass);
+        confirmpass = findViewById(R.id.confirmpass);
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!pass.getText().equals("")){
+                    if(pass.getText().toString().equals(confirmpass.getText().toString())){
+
+                    }
+                }
+            }
+        });
     }
 }
