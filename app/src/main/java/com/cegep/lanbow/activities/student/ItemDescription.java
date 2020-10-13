01d02a35f9eeefd0f430ad2782aea2cd981dd757
Sai Akshay <erupakasaiakshay@gmail.com>
2020-10-13 16:42:31 -0400

    updated and fix item description class

--- app/src/main/java/com/cegep/lanbow/activities/student/ItemDescription.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/ItemDescription.java
@@ -2,9 +2,11 @@ package com.cegep.lanbow.activities.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cegep.lanbow.R;
@@ -15,17 +17,26 @@ public class ItemDescription extends AppCompatActivity {
     private TextView ItemName;
     private TextView ItemDes;
     private Button Reserve;
+    private ImageView backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_description);
 
-        Item item = (Item) getIntent().getSerializableExtra("data");
+        final Item item = (Item) getIntent().getSerializableExtra("data");
 
         ItemName = findViewById(R.id.itemName);
         ItemDes = findViewById(R.id.itemDes);
         Reserve = findViewById(R.id.reserve);
+        backbtn = findViewById(R.id.backbtn);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         ItemName.setText(item.getItemName());
         ItemDes.setText(item.getItemDes());
@@ -33,7 +44,9 @@ public class ItemDescription extends AppCompatActivity {
         Reserve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent in = new Intent(ItemDescription.this,Reservation.class);
+                in.putExtra("data",item);
+                startActivity(in);
             }
         });
 
