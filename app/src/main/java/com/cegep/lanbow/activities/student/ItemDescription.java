87ba3d82b4ed97c7d2dcfa490a3287e74faf9275
Sai Akshay <erupakasaiakshay@gmail.com>
2020-10-13 10:50:58 -0400

    added ui for item description activity

---
+++ app/src/main/java/com/cegep/lanbow/activities/student/ItemDescription.java
@@ -0,0 +1,16 @@
+package com.cegep.lanbow.activities.student;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cegep.lanbow.R;
+
+public class ItemDescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_description);
+    }
+}
