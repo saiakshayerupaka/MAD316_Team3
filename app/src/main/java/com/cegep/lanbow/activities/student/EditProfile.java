9983f526775553d890d8dcaaba80eeae1b10f494
AmanSehmi <amansehmi10@gmail.com>
2020-10-13 21:22:54 -0400

    update edit profile

--- app/src/main/java/com/cegep/lanbow/activities/student/EditProfile.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/EditProfile.java
@@ -3,14 +3,49 @@ package com.cegep.lanbow.activities.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.cegep.lanbow.R;
 
 public class EditProfile extends AppCompatActivity {
 
+    private ImageView backbtn;
+    private EditText email;
+    private EditText name;
+    private EditText phone;
+    private EditText address;
+    private Button save;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        backbtn = findViewById(R.id.backbtn);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        email = findViewById(R.id.emailInput);
+        name = findViewById(R.id.nameInput);
+        phone = findViewById(R.id.phoneInput);
+        address = findViewById(R.id.addressInput);
+
+        save = findViewById(R.id.save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
