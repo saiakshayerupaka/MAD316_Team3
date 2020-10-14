671e3f78c25a832d88d11a8febff7ef2b81ff588
AmanSehmi <amansehmi10@gmail.com>
2020-10-13 21:14:39 -0400

    added alert dialog for password reset link

--- app/src/main/java/com/cegep/lanbow/activities/student/UserProfile.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/UserProfile.java
@@ -1,12 +1,16 @@
 package com.cegep.lanbow.activities.student;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cegep.lanbow.R;
@@ -22,7 +26,9 @@ public class UserProfile extends AppCompatActivity {
     private TextView name,email,studentid,phone,address;
     private FirebaseAuth auth;
     private FirebaseDatabase database;
-    private Button editprofile;
+    private Button resetpassword;
+    private Button editProfile;
+    private ImageView backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +38,14 @@ public class UserProfile extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
 
+        backbtn = findViewById(R.id.backbtn);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
         name = findViewById(R.id.username);
         email = findViewById(R.id.email);
@@ -39,16 +53,25 @@ public class UserProfile extends AppCompatActivity {
         phone = findViewById(R.id.phonenumber);
         address = findViewById(R.id.address);
 
-        editprofile = findViewById(R.id.editProfile);
+        resetpassword = findViewById(R.id.changePass);
 
-        editprofile.setOnClickListener(new View.OnClickListener() {
+        resetpassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(UserProfile.this,EditProfile.class);
-                startActivity(intent);
+                auth.sendPasswordResetEmail(auth.getCurrentUser().getEmail());
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(UserProfile.this);
+                alertDialogBuilder.setMessage("Password reset link has been set to your registered email.");
+                alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.show();
             }
         });
-
 
         email.setText(auth.getCurrentUser().getEmail());
 
@@ -65,6 +88,15 @@ public class UserProfile extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
+            }
+        });
+
+
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserProfile.this,EditProfile.class);
+                startActivity(intent);
             }
         });
 
