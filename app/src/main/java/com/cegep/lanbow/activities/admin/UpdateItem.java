17c78225af9a0ff13f4095b8d4ce7b126ad0d45e
harshavardhan <haiharshavardhan@gmail.com>
2020-10-18 16:24:52 -0400

    created update item screen ui

---
+++ app/src/main/java/com/cegep/lanbow/activities/admin/UpdateItem.java
@@ -0,0 +1,16 @@
+package com.cegep.lanbow.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cegep.lanbow.R;
+
+public class UpdateItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_item);
+    }
+}
