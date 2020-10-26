3d0afc333a5a5209f71cc74b911e51689769356f
Prashant-Nannuru <prashanthreddy.1234@yahoo.in>
2020-10-26 14:29:51 -0400

    added spinner and firebase db

--- app/src/main/java/com/cegep/lanbow/activities/student/Support.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/Support.java
@@ -1,15 +1,20 @@
 package com.cegep.lanbow.activities.student;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.cegep.lanbow.R;
 import com.cegep.lanbow.models.Message;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -34,6 +39,12 @@ public class Support extends AppCompatActivity {
 
         messageTitle = findViewById(R.id.titleInput);
         spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.message_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
         message = findViewById(R.id.msg);
 
         send = findViewById(R.id.send);
@@ -42,7 +53,24 @@ public class Support extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(!messageTitle.getText().equals("") && !message.getText().equals("") && spinner.getSelectedItem()!=null){
-                    database.getReference().child("Messages").child(UUID.randomUUID().toString()).setValue(new Message(messageTitle.getText().toString(),))
+                    database.getReference().child("Messages").child(UUID.randomUUID().toString()).setValue(new Message(messageTitle.getText().toString(),spinner.getSelectedItem().toString(),message.getText().toString(),"unread",firebaseAuth.getCurrentUser().getUid())).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(Support.this,"Message Sent successfully",Toast.LENGTH_LONG).show();
+                                finish();
+                            }
+                            else{
+                                Toast.makeText(Support.this,"Something went wrong!!!",Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                    });
+                }
+
+                else{
+                    Toast.makeText(Support.this,"Something Missing!!",Toast.LENGTH_LONG).show();
+
                 }
 
 
