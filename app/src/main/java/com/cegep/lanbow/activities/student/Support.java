51a78a716936a0e8ed812ad68e278b18263aabf8
harshavardhan <haiharshavardhan@gmail.com>
2020-10-26 13:01:57 -0400

    updated support activity

--- app/src/main/java/com/cegep/lanbow/activities/student/Support.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/Support.java
@@ -3,6 +3,8 @@ package com.cegep.lanbow.activities.student;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -12,11 +14,31 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Support extends AppCompatActivity {
 
-
+    private EditText messageTitle,message;
+    private Spinner spinner;
+    private FirebaseDatabase database;
+    private FirebaseAuth firebaseAuth;
+    private Button send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+
+        messageTitle = findViewById(R.id.titleInput);
+        spinner = findViewById(R.id.spinner);
+        message = findViewById(R.id.msg);
+
+        send = findViewById(R.id.send);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
