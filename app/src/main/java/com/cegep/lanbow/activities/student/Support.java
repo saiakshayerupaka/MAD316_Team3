01b259526c5ccccf8858e2795c36816d4910e9f4
Prashant-Nannuru <prashanthreddy.1234@yahoo.in>
2020-10-26 14:23:11 -0400

    added model class for message

--- app/src/main/java/com/cegep/lanbow/activities/student/Support.java
+++ app/src/main/java/com/cegep/lanbow/activities/student/Support.java
@@ -9,8 +9,11 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.cegep.lanbow.R;
+import com.cegep.lanbow.models.Message;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class Support extends AppCompatActivity {
 
@@ -25,6 +28,10 @@ public class Support extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
 
+        database = FirebaseDatabase.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
         messageTitle = findViewById(R.id.titleInput);
         spinner = findViewById(R.id.spinner);
         message = findViewById(R.id.msg);
@@ -34,6 +41,10 @@ public class Support extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!messageTitle.getText().equals("") && !message.getText().equals("") && spinner.getSelectedItem()!=null){
+                    database.getReference().child("Messages").child(UUID.randomUUID().toString()).setValue(new Message(messageTitle.getText().toString(),))
+                }
+
 
             }
         });
---
+++ app/src/main/java/com/cegep/lanbow/models/Message.java
@@ -0,0 +1,64 @@
+package com.cegep.lanbow.models;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+
+    public Message() {
+    }
+
+    public String getMessageTitle() {
+        return messageTitle;
+    }
+
+    public void setMessageTitle(String messageTitle) {
+        this.messageTitle = messageTitle;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageStatus() {
+        return messageStatus;
+    }
+
+    public void setMessageStatus(String messageStatus) {
+        this.messageStatus = messageStatus;
+    }
+
+    public String getMessageBy() {
+        return messageBy;
+    }
+
+    public void setMessageBy(String messageBy) {
+        this.messageBy = messageBy;
+    }
+
+    public Message(String messageTitle, String messageType, String message, String messageStatus, String messageBy) {
+        this.messageTitle = messageTitle;
+        this.messageType = messageType;
+        this.message = message;
+        this.messageStatus = messageStatus;
+        this.messageBy = messageBy;
+    }
+
+    private String messageTitle;
+    private String messageType;
+    private String message;
+    private String messageStatus;
+    private String messageBy;
+
+}
