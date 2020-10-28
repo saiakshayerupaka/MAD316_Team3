1577cf090065314ad6399283b22984672855f5a0
AmanSehmi <amansehmi10@gmail.com>
2020-10-27 23:33:44 -0400

    added ui for message info

---
+++ app/src/main/java/com/cegep/lanbow/activities/admin/MessageInfo.java
@@ -0,0 +1,16 @@
+package com.cegep.lanbow.activities.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cegep.lanbow.R;
+
+public class MessageInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_info);
+    }
+}
